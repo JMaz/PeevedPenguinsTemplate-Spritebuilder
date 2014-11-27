a7507e0748c084a650d776d25f1e56a5a5f62a72
JMaz <janmasztal@mail.adelphi.edu>
2014-11-27 12:23:27 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Penguin.m
@@ -0,0 +1,14 @@
+//
+//  Penguin.m
+//  PeevedPenguins
+//
+//  Created by Athena on 11/27/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Penguin.h"
+
+@implementation Penguin
+
+
+@end
---
+++ PeevedPenguins.spritebuilder/Seal.m
@@ -0,0 +1,13 @@
+//
+//  Seal.m
+//  PeevedPenguins
+//
+//  Created by Athena on 11/27/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Seal.h"
+
+@implementation Seal
+
+@end
