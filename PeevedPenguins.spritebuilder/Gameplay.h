152c459a83e7ea0e81842ce351d2f1c8bca72650
JMaz <janmasztal@mail.adelphi.edu>
2014-11-27 09:33:20 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Gameplay.h
@@ -0,0 +1,12 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Athena on 11/27/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay:CCNode
+@end
